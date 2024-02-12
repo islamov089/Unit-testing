7aa0bdae8f0e2718a872b676a1016b8a0cc12056
Ilhom <Ilhom.Islomov@inoviagroup.se>
2024-02-12 14:29:44 +0500

    fix

--- intro/checkAge.js
+++ intro/checkAge.js
@@ -1,4 +1,4 @@
-function checkCondition(people) {
+function checkAgeInObject(people) {
     return people.every(person => person.age >= 18);
   }
-   module.exports = checkCondition;
+   module.exports = checkAgeInObject
--- intro/checkDivide.js
+++ intro/checkDivide.js
@@ -2,7 +2,4 @@ function checkToDivide(num) {
   return typeof num === "number" && num % 3 === 0 && num % 5 === 0;
 }
 
-console.log(checkToDivide(15));
-console.log(checkToDivide(30));
-
 module.exports = checkToDivide;
